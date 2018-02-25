9cd332e438b385f8684b21a6415b033d88de73f8
egusahiroaki <egusa.hiroaki@gmail.com>
2018-02-25 22:53:12 +0900

    change format

--- origin/index.js
+++ origin/index.js
@@ -59,11 +59,25 @@ var Ball;
 
 }());
 
-var alice = new Person("Alice");
+function init(personName, dogName, ballColor){
+  var alice = new Person(personName);
 
-console.log(alice.sayName());
+  console.log(alice.sayName());
 
-var newPet = new Dog();
-alice.keep(newPet);
-alice.namePet("Max");
-console.log(newPet.say());
+  var newPet = new Dog();
+  alice.keep(newPet);
+  alice.namePet(dogName);
+  console.log(newPet.say());
+  console.log(newPet.sayOutside());
+  var ball = new Ball(ballColor);
+  newPet.find(ball);
+  ball.rollOver();
+  newPet.find(ball);  
+}
+
+// Alice
+// Alice さんのペットの名前は、 Max です
+// わんわん!!
+// 家の外からわんわん!!
+// 犬はうろうろしているよ!
+// 赤色のボールを見つけたよ!
