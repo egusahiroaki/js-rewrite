b3821db876a1704cc03523d9efcd240acc365117
egusahiroaki <egusa.hiroaki@gmail.com>
2018-02-25 21:46:36 +0900

    add webpack config

---
+++ step1/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src'),
+    }]
+  },
+  devServer: {
+    watchContentBase: true,
+    port: 8080,
+  },
+};
