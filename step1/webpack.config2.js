907601b5084bfdab229237f6bc2489c3cc37b60c
egusahiroaki <egusa.hiroaki@gmail.com>
2018-11-29 13:56:28 +0900

    add

---
+++ step1/webpack.config2.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }]
+    }]
+  },
+  devServer: {
+    watchContentBase: true,
+    port: 8080,
+  },
+};
